20818a39011480a2b3b8055e5bc5eb26a76cb650
Spartan Software Enterprises <opensource@spartan-gaming.com>
2026-08-13 03:26:54 +0000

    feat: Initial desktop project scaffold - Electron + React setup, project structure, package.json, main.js, index.html, prettier config

---
+++ src/main/main.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+  win.loadFile("src/renderer/index.html");
+}
+
+app.whenReady().then(() => {
+  createWindow();
+});
